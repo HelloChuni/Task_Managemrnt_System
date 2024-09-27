158ed3369e4e6dd5145a989be125d18aa65f993c
HelloChuni <in.arpita.pati@gmail.com>
2024-09-27 17:57:39 +0530

    Add backend of task management

---
+++ src/main/java/com/code/Task_SpringbBoot/TaskSpringbBootApplication.java
@@ -0,0 +1,13 @@
+package com.code.Task_SpringbBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSpringbBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSpringbBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code/Task_SpringbBoot/TaskSpringbBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.Task_SpringbBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSpringbBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
